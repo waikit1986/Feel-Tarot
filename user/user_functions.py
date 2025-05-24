3314419bd3b785d65126d5b64986e18226ca5245
waikit <waikit6461@gmail.com>
2025-05-24 17:47:40 +0800

    username

--- user/user_functions.py
+++ user/user_functions.py
@@ -28,14 +28,14 @@ def get_user_by_username(db: Session, username: str):
   user = db.query(User).filter(User.username == username).first()
   if not user:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-      detail='User with username {username} not found')
+      detail=f'User with username {username} not found')
   return user
 
 def update_user(db: Session, username: str, request: UserBase):
   user = db.query(User).filter(User.username == username)
   if not user.first():
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-      detail='User with username {username} not found')
+      detail=f'User with username {username} not found')
   user.update({
     User.username: request.username,
     User.email: request.email,
