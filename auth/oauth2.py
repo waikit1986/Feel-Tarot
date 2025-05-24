52e693d84bcf28fa10cecba59b1a3bc18175ae17
waikit <waikit6461@gmail.com>
2025-05-24 20:35:04 +0800

    1min

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -20,7 +20,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
   if expires_delta:
     expire = datetime.now(timezone.utc) + expires_delta
   else:
-    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
   to_encode.update({"exp": expire})
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
